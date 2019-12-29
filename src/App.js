d119d846afe65f896c902bb1e9586827b6185d84
silvonei <silvoneildr@gmail.com>
2019-12-29 09:43:21 -0300

    Init app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Main from './pages/Main';
+
+import GlobalStyles from './styles/global';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Main />
+    </>
+  );
+}
+
+export default App;
