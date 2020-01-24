cb53114d484d8d5a5596a864ee9a71721e91f2ba
silvonei <silvoneildr@gmail.com>
2020-01-24 09:44:28 -0300

    Update Settings

--- public/electron.js
+++ public/electron.js
@@ -1,11 +1,4 @@
-const {
-  app,
-  BrowserWindow,
-  dialog,
-  ipcMain,
-  Menu,
-  Tray,
-} = require('electron');
+const { app, BrowserWindow, dialog, ipcMain, Menu, Tray } = require('electron');
 
 const path = require('path');
 const isDev = require('electron-is-dev');
@@ -21,81 +14,83 @@ function forceCloseMainWindow() {
 }
 
 function renderTrayBar(mainWindow) {
-	iconPath = path.join(__dirname, './iconclock.png');
+  iconPath = path.join(__dirname, './iconclock.png');
   tray = new Tray(iconPath);
-	contextMenu = Menu.buildFromTemplate([
-		{
-			label: 'Abrir',
-			click() {
-				mainWindow.show();
-			}
+  contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Abrir',
+      click() {
+        mainWindow.show();
+      },
     },
     {
-			label: 'Minimizar',
-			click() {
-				mainWindow.minimize();
-			}
-		},
+      label: 'Minimizar',
+      click() {
+        mainWindow.minimize();
+      },
+    },
     {
-			label: 'Maximizar',
-			click() {
-				mainWindow.maximize();
-			}
-		},
-		{
-			label: 'Fechar',
-			click() {
+      label: 'Maximizar',
+      click() {
+        mainWindow.maximize();
+      },
+    },
+    {
+      label: 'Fechar',
+      click() {
         forceCloseMainWindow();
-			}
-		}
-	]);
+      },
+    },
+  ]);
 
-	tray.setToolTip('Time Show Electron.');
-	tray.setContextMenu(contextMenu);
+  tray.setToolTip('Time Show Electron.');
+  tray.setContextMenu(contextMenu);
 }
 
 function createWindow() {
-	mainWindow = new BrowserWindow({
-		show: false,
-		width: 1024,
-		height: 720,
-		webPreferences: {
-			nodeIntegration: true
-		}
-	});
+  mainWindow = new BrowserWindow({
+    show: false,
+    width: 1024,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
-	mainWindow.loadURL(
-		isDev ? 'http://localhost:3000' : `file://${path.resolve(__dirname, '..', 'build', 'index.html')}`
-	);
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.resolve(__dirname, '..', 'build', 'index.html')}`,
+  );
 
-	if (isDev) {
-		mainWindow.webContents.openDevTools();
-	}
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
-	mainWindow.setMenuBarVisibility(false);
+  mainWindow.setMenuBarVisibility(false);
 
-	mainWindow.on('closed', () => {
-		mainWindow = null;
-	});
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 
-	mainWindow.once('ready-to-show', () => {
-		mainWindow.show();
-		renderTrayBar(mainWindow);
-	});
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+    renderTrayBar(mainWindow);
+  });
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-	if (mainWindow === null) {
-		createWindow();
-	}
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
 
 function onCloseWindow(e, options) {
@@ -106,21 +101,24 @@ function onCloseWindow(e, options) {
     e.preventDefault();
     forceCloseMainWindow();
   }
- }
+}
 
 ipcMain.on('confirm-close', (event, options) => {
   onCloseWindow(event, options);
 });
 
 function openFileToRenderer(event) {
-  dialog.showOpenDialog(mainWindow, {
-    properties: ['openFile', 'openDirectory']
-  }).then(result => {
-    console.log(result.canceled)
-    console.log(result.filePaths)
-  }).catch(err => {
-    console.log(err)
-  })
+  dialog
+    .showOpenDialog(mainWindow, {
+      properties: ['openFile', 'openDirectory'],
+    })
+    .then(result => {
+      console.log(result.canceled);
+      console.log(result.filePaths);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 ipcMain.on('open-file', event => {
